917f1833244858535fc5f29f617721b8a0e23db5
Oskar <oskartdragon@gmail.com>
2024-02-20 10:30:49 +0000

    feat: add changes

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>
+          I changed code in a React App
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
